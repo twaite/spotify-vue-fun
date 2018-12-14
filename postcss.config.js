4331272f8f0db4e719aa17dfd0a0e0b4dbe701dc
Tim Waite <tawaite@gmail.com>
2018-12-13 23:58:47 -0600

    Remove purgcss until whitelisting works

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,20 @@
-const { whitelistPatterns, whitelistedPatternsChilden } = require('./src/whitelisted-classes');
-const purgecss = require('@fullhuman/postcss-purgecss');
+// const { whitelistPatterns, whitelistedPatternsChilden } = require('./src/whitelisted-classes');
+// const purgecss = require('@fullhuman/postcss-purgecss');
 const tailwindcss = require('tailwindcss');
-const isDev = process.argv.indexOf('serve') !== -1;
+// const isDev = process.argv.indexOf('serve') !== -1;
+
+// TODO: tw - fix purgecss
 
 module.exports = {
   plugins: [
     tailwindcss('tailwind.js'),
-    isDev
-      ? ''
-      : purgecss({
-        content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.scss'],
-        whitelistPatterns,
-        whitelistedPatternsChilden
-      }),
+    // isDev
+    //   ? ''
+    //   : purgecss({
+    //     content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.scss'],
+    //     whitelistPatterns,
+    //     whitelistedPatternsChilden
+    //   }),
     require('autoprefixer')
   ]
 };
