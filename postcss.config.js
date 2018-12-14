3504d62a8143eb20d95e40025e48b7fb46c01967
Tim Waite <tawaite@gmail.com>
2018-12-13 22:58:16 -0600

    purgecss added

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,15 @@
-// const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require('@fullhuman/postcss-purgecss');
 const tailwindcss = require('tailwindcss');
-// const isDev = process.argv.indexOf('serve') !== -1;
-// TODO: tw - figure out why purgecss is missing stuff on build
+const isDev = process.argv.indexOf('serve') !== -1;
 
 module.exports = {
   plugins: [
     tailwindcss('tailwind.js'),
-    // isDev
-    //   ? ''
-    //   : purgecss({
-    //       content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.scss']
-    //     }),
+    isDev
+      ? ''
+      : purgecss({
+        content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.scss']
+      }),
     require('autoprefixer')
   ]
 };
--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <h1 class="text-red">Hello World</h1>
     <router-view/>
   </div>
 </template>
