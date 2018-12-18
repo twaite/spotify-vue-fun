95bf9289b709a748b34d3c93b0021cb15ba5dbd9
Tim Waite <tim@dialexa.com>
2018-12-18 00:08:05 -0600

    Add netlify config

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import LoginPage from '@/pages/login-page';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -22,3 +22,24 @@ export default new Router({
     // }
   ]
 });
+
+// function getHashParams() {
+
+//   var hashParams = {};
+//   var e,
+//       a = /\+/g,  // Regex for replacing addition symbol with a space
+//       r = /([^&;=]+)=?([^&;]*)/g,
+//       d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+//       q = window.location.hash.substring(1);
+
+//   while (e = r.exec(q))
+//     hashParams[d(e[1])] = d(e[2]);
+
+//   return hashParams;
+// }
+
+// router.beforeEach((to) => {
+  
+// });
+
+export default router;
